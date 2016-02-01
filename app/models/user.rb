a78216c715e9613e8403407433a1401a881731b5
Christopher Baxter <swimn10s@gmail.com>
2016-01-31 19:59:30 -0500

    Add pins/users association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ActiveRecord::Base
 	validates :password, :presence => true,
                      :on => :create,
                      :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ }
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  	# Include default devise modules. Others available are:
+  	# :confirmable, :lockable, :timeoutable and :omniauthable
+  	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+	has_many :pins       
 end
